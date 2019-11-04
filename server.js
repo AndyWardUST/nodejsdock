71e923265ba5083fff0b3e654fc3a197fe95b982
AndyWardUST <57137718+andywardust@users.noreply.github.com>
2019-11-04 15:32:18 +0000

    mod server message

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello to Max\n');
+  res.send('Hello all, complete end to end\n');
 });
 
 app.listen(PORT, HOST);
