77c3287366d428902845b21cb867162df7fd290a
AndyWardUST <57137718+andywardust@users.noreply.github.com>
2019-11-06 11:21:47 +0000

    adding api pshell and output 12

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello all, complete end to end\n');
+  res.send('Hello all, now trying own agent\n');
 });
 
 app.listen(PORT, HOST);
