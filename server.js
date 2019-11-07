5dc860606d9fb730a4a8a2f6f7e20f52185252b6
AndyWardUST <andy.ward@btinternet.com>
2019-11-07 17:15:18 +0000

    full run with cmx :)

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello all, now trying own agent, again\n');
+  res.send('Hello all, the full end to end\n');
 });
 
 app.listen(PORT, HOST);
