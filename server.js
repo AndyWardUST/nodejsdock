98103f9d6d05e0eb197606b98e9786cb4e4b3a3b
Eldarrin <andy.ward@btinternet.com>
2019-11-28 19:40:04 +0900

    update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello all, I hope you like this video\n');
+  res.send('Hello all, releaseing full pipeline\n');
 });
 
 app.listen(PORT, HOST);
